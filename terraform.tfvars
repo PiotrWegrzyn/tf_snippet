f3085e16a1c224b9b87daba9ae312ea900429c7c
Piotr Wegrzyn <pwegrzyn@axpo.pl>
2026-01-22 10:18:51 +0100

    Initial Commit

---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+project_id  = "ws-piotr-wegrzyn"
+
+region      = "europe-west1"
+environment = "dev"
+
+labels = {
+  owner = "team"
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
-  default     = "ws-piotr-wegrzyn"
 }
 
 variable "region" {
